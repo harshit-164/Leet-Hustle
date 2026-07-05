c2d2522267c75da4821f0653b6b7b6d297bdf8fc
Harshit Trivedi <harshittrivedi164sharma@gmail.com>
2026-07-05 22:36:45 +0530

    Time: 0 ms (100%), Space: 52.2 MB (97.55%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+function getRow(rowIndex) {
+    let row = [1];
+    for (let i = 1; i <= rowIndex; i++) {
+        row.push(1);
+        for (let j = i - 1; j > 0; j--) {
+            row[j] = row[j] + row[j - 1];
+        }
+    }
+
+    return row;
+}
